6ac55a18a652b63073cf38003443ecd62c020352
N1ghtmarex <andreyshe22@gmail.com>
2026-01-22 18:35:58 +0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  Добро пожаловать, {{ userData }}
+</template>
+
+<script>
+import axios from 'axios';
+
+//import WebApp from '@twa-dev/sdk'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      userData: null,
+    }
+  },
+  async mounted() {
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>

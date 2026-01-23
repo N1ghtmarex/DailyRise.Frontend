87856975d11c39d89c77a3a8469359b314d21080
N1ghtmarex <andreyshe22@gmail.com>
2026-01-23 21:07:13 +0700

    refactor: добавлена карточка испытания * Код карточек испытаний вынесен в отдельный компонент

---
+++ src/components/ChallengeCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="challenge-card">
+        <div class="challenge-header">
+            <div class="title">
+                <h3 class="name">{{ challenge.name }}</h3>
+                <p class="description">{{ challenge.description }}</p>
+            </div>
+        </div>
+        <div class="progress-wrapper">
+            <div class="progress-container">
+                <div class="days-progress">
+                    <span class="days">День {{ Math.round((new Date(new Date() - Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1 }} из {{ Math.round((new Date(Date.parse(challenge.endDate)) - new Date(Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) }}</span>
+                    <span class="days-percent">
+                        {{ 
+                        Math.round((Math.round((new Date(new Date() - Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1) /
+                            (Math.round((new Date(Date.parse(challenge.endDate)) - new Date(Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)))) * 100
+                        }}%
+                    </span>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar" :style="`width: ${Math.round((Math.round((new Date(new Date() - Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1) /
+                            (Math.round((new Date(Date.parse(challenge.endDate)) - new Date(Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)))) * 100}%`"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    props: ['challenge'],
+    components: {
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,68 +8,19 @@
         </div>
     </div>
     <div class="challenge-container">
-        <div class="challenge-card" v-if="filter == 'all'" v-for="challenge in challenges.items">
-            <div class="challenge-header">
-                <div class="title">
-                    <h3 class="name">{{ challenge.name }}</h3>
-                    <p class="description">{{ challenge.description }}</p>
-                </div>
-            </div>
-
-            <div class="progress-wrapper">
-                <div class="progress-container">
-                    <div class="days-progress">
-                        <span class="days">День {{ Math.round((new Date(new Date() - Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1 }} из {{ Math.round((new Date(Date.parse(challenge.endDate)) - new Date(Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) }}</span>
-                        <span class="days-percent">
-                            {{ 
-                            Math.round((Math.round((new Date(new Date() - Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1) /
-                             (Math.round((new Date(Date.parse(challenge.endDate)) - new Date(Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)))) * 100
-                            }}%
-                        </span>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" :style="`width: ${Math.round((Math.round((new Date(new Date() - Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1) /
-                             (Math.round((new Date(Date.parse(challenge.endDate)) - new Date(Date.parse(challenge.startDate))) / (1000 * 60 * 60 * 24)))) * 100}%`"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="challenge-card" v-if="filter == 'my'" v-for="item in userChallenges.items">
-            <div class="challenge-header">
-                <div class="title">
-                    <h3 class="name">{{ item.challenge.name }}</h3>
-                    <p class="description">{{ item.challenge.description }}</p>
-                </div>
-            </div>
-
-            <div class="progress-wrapper">
-                <div class="progress-container">
-                    <div class="days-progress">
-                        <span class="days">День {{ Math.round((new Date(new Date() - Date.parse(item.challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1 }} из {{ Math.round((new Date(Date.parse(item.challenge.endDate)) - new Date(Date.parse(item.challenge.startDate))) / (1000 * 60 * 60 * 24)) }}</span>
-                        <span class="days-percent">
-                            {{ 
-                            Math.round((Math.round((new Date(new Date() - Date.parse(item.challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1) /
-                             (Math.round((new Date(Date.parse(item.challenge.endDate)) - new Date(Date.parse(item.challenge.startDate))) / (1000 * 60 * 60 * 24)))) * 100
-                            }}%
-                        </span>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" :style="`width: ${Math.round((Math.round((new Date(new Date() - Date.parse(item.challenge.startDate))) / (1000 * 60 * 60 * 24)) + 1) /
-                             (Math.round((new Date(Date.parse(item.challenge.endDate)) - new Date(Date.parse(item.challenge.startDate))) / (1000 * 60 * 60 * 24)))) * 100}%`"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ChallengeCard v-if="filter == 'all'" v-for="item in challenges.items" :challenge="item"></ChallengeCard>
+        <ChallengeCard v-if="filter == 'my'" v-for="item in userChallenges.items" :challenge="item.challenge"></ChallengeCard>
     </div>
 </template>
 
 <script>
+import ChallengeCard from '@/components/ChallengeCard.vue';
 import axios from '@/plugins/axios';
 
 
 export default {
     components: {
+        ChallengeCard,
     },
     data() {
         return {

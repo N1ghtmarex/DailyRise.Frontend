c7f3cbe968c50c6ef666d0076b236930b3ec7477
N1ghtmarex <andreyshe22@gmail.com>
2026-02-06 17:31:13 +0700

    refactor: изменены стили + Добавлены отдельные стили для хедера и навбара * Применены белые стили

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav>
-        <button :class="this.$route.name == 'Home' ? 'active' : ''" @click="this.$router.push('/')">🏆<br>Вызовы</button>
+        <button :class="this.$route.name == 'Home' ? 'active' : ''" @click="this.$router.push('/')">🏆<br>Соревнования</button>
         <button :class="this.$route.name == 'Test' ? 'active' : ''" @click="this.$router.push('/test')">👥<br>Друзья</button>
         <button>👤<br>Профиль</button>
     </nav>
